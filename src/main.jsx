2b38c6c7dd51b25050ebb981af5b2cd163b7727d
रजव | <razeev.puree@gmail.com>
2025-03-18 13:46:22 +0000

    refactor: simplify main.jsx by removing router setup and using App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+// import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 import "./index.css";
-import { routeTree } from "./routeTree.gen";
+import App from "./App";
+// import { routeTree } from "./routeTree.gen";
 
 // Initialize the router with your route tree
-const router = createRouter({
-  routeTree,
-  defaultPreload: 'intent',
-  defaultLinkPreloadMaxAge: 5000,
-  basepath: import.meta.env.BASE_URL || "",
-});
-
+// const router = createRouter({
+//   routeTree,
+//   defaultPreload: 'intent',
+//   defaultLinkPreloadMaxAge: 5000,
+//   basepath: import.meta.env.BASE_URL || "",
+// });
 
 // Create root and render the app with RouterProvider
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
+
+    {/* <RouterProvider router={router} /> */}
   </StrictMode>
 );
