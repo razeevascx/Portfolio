c27a5ac2eef2871edfbfabe19384f8d44179ce04
Rajeev Puri <razeev.puree@gmail.com>
2025-08-14 15:37:34 +0100

    Files and dep setup for next js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import viteReact from "@vitejs/plugin-react";
-import mdx from '@mdx-js/rollup';
-
-export default defineConfig({
-  base: "./",
-  plugins: [viteReact(),mdx()],
-});
