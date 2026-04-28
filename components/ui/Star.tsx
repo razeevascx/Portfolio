4741109e30fdf797535e0cb9998dd4a88ccd2564
Rajeev Puri <razeev.puree@gmail.com>
2026-04-28 23:38:15 +0100

    Remove unused components, assets, and lock file cleanup

--- components/ui/Star.tsx
+++
@@ -1,9 +0,0 @@
-import { useRef } from "react";
-// THREE.js component disabled - not installed and not used
-
-const StarsCanvas = () => {
-  // Component disabled - THREE.js not installed
-  return null;
-};
-
-export default StarsCanvas;
