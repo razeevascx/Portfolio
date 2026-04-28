e1d0f7106dbd219d93fb8dd497d9695d785b362c
Rajeev Puri <razeev.puree@gmail.com>
2026-04-28 23:37:52 +0100

    Refactor and extend card components with new certificate and language cards

--- components/cards/SkillCard.tsx
+++ components/cards/SkillCard.tsx
@@ -2,41 +2,30 @@
 
 import * as motion from "motion/react-client";
 import { type Skill } from "@/lib/skills-data";
+import { cn } from "@/lib/utils";
 
 interface SkillCardProps {
   skill: Skill;
-  isHovered: boolean;
-  onHover: () => void;
-  onLeave: () => void;
+  className?: string;
 }
 
-const SkillCard = ({ skill, isHovered, onHover, onLeave }: SkillCardProps) => {
+const SkillCard = ({ skill, className }: SkillCardProps) => {
   return (
     <motion.div
-      className="transition-all duration-300"
-      onMouseEnter={onHover}
-      onMouseLeave={onLeave}
+      className={cn(
+        "gap-4 p-4  border transition-colors duration-300 flex items-center justify-center bg-background px-4 py-8 md:p-8 hover:bg-white/8  group select-none cursor-pointer",
+        className,
+      )}
       initial={{ opacity: 0, y: 10 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.2 }}
     >
-      <div className="flex items-center gap-2">
-        <motion.div
-          className={`text-xl transition-colors duration-300 ${skill.color}`}
-          animate={{ rotate: isHovered ? 360 : 0 }}
-          transition={{ duration: 0.3 }}
-        >
-          {skill.icon}
-        </motion.div>
-
-        <h4
-          className={`text-sm font-light transition-colors duration-300
-            ${isHovered ? "text-color-primary" : "text-color-text-primary"}
-          `}
-        >
-          {skill.label}
-        </h4>
+      <div className="transition-transform duration-500 group-hover:scale-110 ">
+        {skill.icon}
       </div>
+      <h3 className="text-xl font-bold text-white tracking-tight group-hover:text-blue-400 transition-colors">
+        {skill.label}
+      </h3>
     </motion.div>
   );
 };
